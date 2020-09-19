b0230ac1b8d83ac1e8a03a5753b4a85a54dedbf6
Josué Gimenes <josuegalvares@gmail.com>
2020-09-19 18:05:15 -0300

    Desafio concluído

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,86 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    const checkProductsList = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (!checkProductsList.length) {
+      throw new AppError('Invalid Products. Please check the provided list.');
+    }
+
+    const productsListIds = checkProductsList.map(product => product.id);
+
+    const inexistentProducts = products.filter(
+      product => !productsListIds.includes(product.id),
+    );
+
+    if (inexistentProducts.length) {
+      throw new AppError(`Could not find product ${inexistentProducts[0].id}`);
+    }
+
+    function hasDuplicates(list: any): boolean {
+      return new Set(list).size !== list.length;
+    }
+
+    const duplicates = hasDuplicates(checkProductsList);
+
+    if (duplicates) {
+      throw new AppError(
+        'Invalid Products List. Check if is there any duplicated product.',
+      );
+    }
+
+    const productsWithNotEnoughStock = products.filter(
+      product =>
+        checkProductsList.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (productsWithNotEnoughStock.length) {
+      throw new AppError(
+        `The quantity ${productsWithNotEnoughStock[0].quantity} is not available for product ${productsWithNotEnoughStock[0].id}. `,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: checkProductsList.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: serializedProducts,
+    });
+
+    const updatedProductsQuantities = products.map(product => ({
+      id: product.id,
+      quantity:
+        checkProductsList.filter(p => p.id === product.id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(updatedProductsQuantities);
+
+    return order;
   }
 }
 
